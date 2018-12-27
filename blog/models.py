e4dcf3ade4d5dd592bf3d870739d40bd624b146b
Davis JR <davisjr2000@hotmail.com>
2018-12-27 17:53:22 -0200

    Add blog template

--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    title = models.CharField(max_length=100)
     content = models.CharField(max_length=500)
     pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+      return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.template import loader
 from django.http import HttpResponse
-
+from .models import Post
 
 def index(request):
-    return HttpResponse("Hello, world. Welcome to my blog")
+    posts = Post.objects.all
+    template = loader.get_template('index.html')
+    context = {
+     'posts': posts,
+    }
+    return HttpResponse(template.render(context,request))
